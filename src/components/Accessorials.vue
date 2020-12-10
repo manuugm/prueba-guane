c4b6341d9751739a47aec0cb5ed1eb6df865a5c3
Manuela González <manuu-gonzalez@hotmail.com>
2020-12-10 11:29:53 -0500

    Create Accessorials component

---
+++ src/components/Accessorials.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="accessorials">
+    <h2 class="accessorials__title">
+      Accessorials
+    </h2>
+    <hr />
+    <div class="accessorials__options">
+      <b-form-group>
+        <b-form-checkbox-group
+          id="accessorials"
+          :options="options"
+          name="accessorials"
+        ></b-form-checkbox-group>
+      </b-form-group>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Accessorials",
+  data() {
+    return {
+      options: [
+        {
+          text: "Hazmat",
+          value: "hazmat",
+        },
+        {
+          text: "Bonded",
+          value: "bonded",
+        },
+        {
+          text: "Tarps",
+          value: "tarps",
+        },
+        {
+          text: "Airport Delivery Non TSA",
+          value: "airportDeliveryNonTsa",
+        },
+        {
+          text: "Airport Delivery TSA",
+          value: "airportDeliveryTsa",
+        },
+        {
+          text: "Oversize/Overweight Permits",
+          value: "oversizeOverweight",
+        },
+        {
+          text: "Escorts",
+          value: "escorts",
+        },
+        {
+          text: "Team Drivers",
+          value: "teamDrivers",
+        },
+        {
+          text: "Non Stackable",
+          value: "nonStackable",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.accessorials {
+  background-color: #ffffff;
+  border-radius: 20px;
+  padding: 10px;
+}
+</style>
--- src/components/Hauls.vue
+++ src/components/Hauls.vue
@@ -4,16 +4,21 @@
       <Track title="Pickup" zip="Zip from" city="City from" type="pickup" />
       <Track title="Delivery" zip="Zip to" city="City to" type="delivery" />
     </div>
+    <div class="hauls__accessorials">
+      <Accessorials />
+    </div>
   </div>
 </template>
 
 <script>
 import Track from "@/components/Track";
+import Accessorials from "@/components/Accessorials";
 
 export default {
   name: "Hauls",
   components: {
     Track,
+    Accessorials,
   },
 };
 </script>
@@ -21,11 +26,13 @@ export default {
 <style scoped lang="scss">
 .hauls {
   background-color: #e2e2e2;
+  padding: 10px 0;
   width: 100vw;
 
   &__track {
     display: flex;
-    padding: 10px 20px;
+    margin-bottom: 20px;
+    padding: 0px 20px;
 
     .track {
       flex-basis: 50%;
@@ -34,6 +41,9 @@ export default {
         margin-right: 30px;
       }
     }
+  }
+  &__accessorials {
+    padding: 0 20px;
   }
 }
 </style>
